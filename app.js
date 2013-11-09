b07b4b0571e6695c652c194df02dca8e3e7de659
Francisco Collao <pcollaog@gmail.com>
2013-11-09 17:11:28 -0300

    Del User route
    Signed-off-by: Francisco Collao <pcollaog@gmail.com>

--- app.js
+++ app.js
@@ -4,7 +4,6 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 var mongoose = require('mongoose');
